5715e58d43a28d4b1a1183a389e768288d531818
Vladimir Samojlov <v-moi@ya.ru>
2019-10-12 15:00:22 +0300

    edit cats

--- blocks/cats/items.php
+++ blocks/cats/items.php
@@ -1,6 +1,6 @@
 <?php $main_cats = get_terms( array( 'taxonomy' => 'cats', 'parent' => 0, 'hide_empty' => false ) );
 foreach ( $main_cats as $main_cat ): ?>
-	<div class="col-md-3">
+	<div class="col-md-3 mb-4">
 		<a href="<?php echo get_term_link($main_cat) ?>">
 			<div class="box" style="background: url('<?php echo carbon_get_term_meta($main_cat->term_id, 'crb_cats_img' ); ?>'); background-size: cover;">
 				<div class="box_bg"></div>
