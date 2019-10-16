0c3c935c6170b04c44bc26383ee3fb2240f653de
Vladimir Samojlov <v-moi@ya.ru>
2019-10-16 17:53:19 +0300

    edit style text cat

--- blocks/cats/items.php
+++ blocks/cats/items.php
@@ -4,7 +4,7 @@ foreach ( $main_cats as $main_cat ): ?>
 		<a href="<?php echo get_term_link($main_cat) ?>">
 			<div class="box" style="background: url('<?php echo carbon_get_term_meta($main_cat->term_id, 'crb_cats_img' ); ?>'); background-size: cover;">
 				<div class="box_bg"></div>
-				<div class="text-white font_size_m font-weight-bold p-relative">
+				<div class="text-center text-white font_size_m font-weight-bold p-relative">
 					<?php echo $main_cat->name ?>		
 				</div>
 			</div>
